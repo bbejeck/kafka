f267f6bfb3b92386c2b40587e52eb063ca4afed9
Bill Bejeck <bill@confluent.io>
2017-12-28 11:54:19 -0500

    trying time sleep for 2 seconds

--- tests/kafkatest/tests/streams/streams_eos_test.py
+++ tests/kafkatest/tests/streams/streams_eos_test.py
@@ -13,12 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 from ducktape.mark.resource import cluster
-from ducktape.mark import ignore
-
-from kafkatest.tests.kafka_test import KafkaTest
 from kafkatest.services.streams import StreamsEosTestDriverService, StreamsEosTestJobRunnerService, \
     StreamsComplexEosTestJobRunnerService, StreamsEosTestVerifyRunnerService, StreamsComplexEosTestVerifyRunnerService
+from kafkatest.tests.kafka_test import KafkaTest
 
 
 class StreamsEosTest(KafkaTest):
@@ -155,6 +154,7 @@ class StreamsEosTest(KafkaTest):
         self.wait_for_startup(monitor1, keep_alive_processor1)
 
     def wait_for_startup(self, monitor, processor):
+        time.sleep(2)
         self.wait_for(monitor, processor, "StateChange: RUNNING -> REBALANCING")
         self.wait_for(monitor, processor, "StateChange: REBALANCING -> RUNNING")
         self.wait_for(monitor, processor, "processed 500 records from topic=data")
