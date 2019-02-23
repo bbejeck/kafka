b9cc124c03e70e287e03aa453d0cf377dd4f5972
Bill Bejeck <bill@confluent.io>
2019-02-23 08:12:09 -0500

    MINOR: revert change didn't work

--- tests/kafkatest/tests/streams/streams_upgrade_test.py
+++ tests/kafkatest/tests/streams/streams_upgrade_test.py
@@ -90,12 +90,10 @@ class StreamsUpgradeTest(Test):
     processed_msg = "processed [0-9]* records"
 
     def perform_broker_upgrade(self, to_version):
-        self.logger.info("First pass bounce - stop all brokers upgrade versions then upgrade")
+        self.logger.info("First pass bounce - rolling broker upgrade")
         for node in self.kafka.nodes:
             self.kafka.stop_node(node)
             node.version = KafkaVersion(to_version)
-
-        for node in self.kafka.nodes:
             self.kafka.start_node(node)
 
     @cluster(num_nodes=6)
